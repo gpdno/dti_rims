9f66d5cd3cf3409dc4b1e8c60e539302aea0b223
gpdno <gdeninno@suncoastde.com>
2017-03-13 19:43:03 -0400

    added parts and projects views

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,7 +1,57 @@
 class PartsController < ApplicationController
+  before_action :set_part, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@parts = Part.all
-	end
+  def index
+    @parts = Part.all
+  end
 
+  def show
+  end
+
+  def new
+    @part = Part.new
+  end
+
+  def edit
+  end
+
+  def create
+    @part = Part.new(part_params)
+
+    respond_to do |format|
+      if @part.save
+        format.html { redirect_to @part, notice: 'Part was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @part.update(part_params)
+        format.html { redirect_to @part, notice: 'Part was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @part.destroy
+    respond_to do |format|
+      format.html { redirect_to parts_url, notice: 'Part was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_part
+      @part = Part.find(params[:id])
+    end
+
+    def part_params
+      params.require(:part).permit(:partno, :description, :rev, project_ids: [])
+    end
 end
