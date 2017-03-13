ba0945814baae78f3fca87624d04a955e5f9b82e
gpdno <gpdno@mac.com>
2017-03-13 17:01:31 -0400

    added basic page layout for home, parts, and project

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,7 +1,7 @@
 class Part < ApplicationRecord
 	validates :partno, presence: true
-	validates :description, presence: true, length: { maximun: 50 }
-	validates :rev, presence: true, only_integer: true
+	validates :description, presence: true, length: { maximum: 50 }
+	validates :rev, presence: true, numericality: { only_integer: true }
 
 	has_many :projectparts
 	has_many :projects, through: :projectparts
